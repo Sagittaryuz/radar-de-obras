eeb7176ee5be9420a636de1e90fdca39412be176
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-08-02 13:10:00 +0000

    se eu ja estiver logado, verifique por nao consigo acessar novamente a p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { getSession } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  const session = await getSession();
-  if (session) {
-    redirect('/dashboard');
-  } else {
-    redirect('/login');
-  }
+  redirect('/dashboard');
 }
