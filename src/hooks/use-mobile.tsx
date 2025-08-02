aec0cbd3d28b293fcd4f356b35a8f74852eebd3b
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-08-02 12:53:57 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,19 +1,20 @@
+"use client"
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
+    const checkIsMobile = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    checkIsMobile() // Set initial value
+    window.addEventListener("resize", checkIsMobile)
+    return () => window.removeEventListener("resize", checkIs-Mobile)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
