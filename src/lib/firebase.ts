477d4d3a91d2fcab0b76fe550d6e8be255f6914a
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-08-05 03:26:42 +0000

    react-dom-client.development.js:25022 Download the React DevTools for a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   projectId: "jcr-radar",
   appId: "1:808852792035:web:f1f4caa30551eb51531bd6",
-  storageBucket: "jcr-radar.appspot.com",
+  storageBucket: "jcr-radar.firebasestorage.app",
   apiKey: "AIzaSyAwY-vS9eyjPHxvcC3as_h5iMwicNRaBqg",
   authDomain: "jcr-radar.firebaseapp.com",
   messagingSenderId: "808852792035"
