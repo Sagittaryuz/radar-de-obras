cac1e4da2eeabbd4c30a111bd4ff3b88a045bfae
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-08-06 01:41:19 +0000

    react-dom-client.development.js:25022 Download the React DevTools for a

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@
 
 import { LoginForm } from '@/app/login/_components/login-form';
 
-export default async function LoginPage() {
+export default function LoginPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <LoginForm />
