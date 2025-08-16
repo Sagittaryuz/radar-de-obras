603a208a6540193c077e0df7e1630de8115353d6
Marcos Rodrigues Pires <mrpires72@gmail.com>
2025-08-16 20:14:52 +0000

    mesmo com essas modificações, o login e senha não funcionam.
    verifique

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   projectId: "jcr-radar",
   appId: "1:808852792035:web:f1f4caa30551eb51531bd6",
-  storageBucket: "jcr-radar.firebasestorage.app",
+  storageBucket: "jcr-radar.appspot.com",
   apiKey: "AIzaSyAwY-vS9eyjPHxvcC3as_h5iMwicNRaBqg",
   authDomain: "jcr-radar.firebaseapp.com",
   messagingSenderId: "808852792035"
